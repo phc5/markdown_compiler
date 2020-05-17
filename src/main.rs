fc0cfada4d257ff39c6292a3517d59d9d939280e
Paul <paulhyunchong@gmail.com>
2020-05-16 22:39:30 -0700

    Strings practice with environment variables

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,31 @@
-use rand::Rng;
-
-fn get_version() -> f32 {
-    return rand::thread_rng().gen_range(0.0, 20.0);
+fn get_title() -> String {
+    let mut title = String::from(env!("CARGO_PKG_NAME"));
+    title.push_str(" (v.");
+    title.push_str(env!("CARGO_PKG_VERSION"));
+    title.push_str(") ");
+    title.push_str(env!("CARGO_PKG_DESCRIPTION"));
+    title
 }
 
-fn usage() {
-    let version: f32 = get_version();
+fn print_short_banner() {
+    println!("{}", get_title());
+}
 
-    println!("Markdown compiler written by Paul Chong");
-    println!("Version {}", version);
+fn print_long_banner() {
+    print_short_banner();
+    println!(
+        "Written by {}\nHomepage: {}\nUsage: mrkdwn <filename>.md",
+        env!("CARGO_PKG_AUTHORS"),
+        env!("CARGO_PKG_HOMEPAGE")
+    );
 }
 
 fn main() {
-    usage();
+    print_long_banner();
 }
+
+// What's happenting here is that the string "0.1" is compiled into
+// the program as a string literal in stack memory. While version
+// is dynamically allocated in heap memory at runtime. Version borrows
+// the value at the address in stack memory.
+// let version = "0.1"
