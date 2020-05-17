7b9d60689cddaeb643190ecfa2b773587491634b
Paul <paulhyunchong@gmail.com>
2020-05-16 22:06:37 -0700

    Adds rand library, function to get random version number

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use rand::Rng;
+
+fn get_version() -> f32 {
+    return rand::thread_rng().gen_range(0.0, 20.0);
+}
+
+fn usage() {
+    let version: f32 = get_version();
+
+    println!("Markdown compiler written by Paul Chong");
+    println!("Version {}", version);
+}
+
 fn main() {
-    println!("Hello, world!");
+    usage();
 }
